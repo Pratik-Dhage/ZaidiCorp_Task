4fe89ca62bbe45b6c5fa3efdad05a61482a00cb3
Pratik Dhage <dhagepratik94@gmail.com>
2023-05-06 02:12:42 +0530

    Successfully getting Distance between Lodha Xperia Mall and My Current Device Location

--- app/src/main/java/com/example/practice_task/MapFragment.java
+++ app/src/main/java/com/example/practice_task/MapFragment.java
@@ -1,18 +1,31 @@
 package com.example.practice_task;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
+import android.provider.Settings;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.practice_task.databinding.FragmentMapBinding;
+import com.example.practice_task.helping_classes.Global;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -26,15 +39,24 @@ import java.util.List;
 public class MapFragment extends Fragment {
 
     FragmentMapBinding binding;
+    LatLng lodhaMallLatLng;
+    LocationManager locationManager;
+    double userLatitude ;
+    double userLongitude ;
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
 
-        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_map, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_map, container, false);
 
-        SupportMapFragment supportMapFragment = (SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.mapFragment);
+        check_If_LocationTurnedOn();
+
+
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapFragment);
 
         assert supportMapFragment != null;
         supportMapFragment.getMapAsync(new OnMapReadyCallback() {
@@ -48,7 +70,7 @@ public class MapFragment extends Fragment {
                 String addressString = "Lodha Xperia Mall, Kalyan Shil Road, Dombivli East, Maharashtra, India";
 
 // Declare a LatLng variable to hold the coordinates
-                LatLng lodhaMallLatLng = null;
+                 lodhaMallLatLng = null;
 
                 try {
                     // Use the Geocoder to get the address information
@@ -80,8 +102,6 @@ public class MapFragment extends Fragment {
 
 
 
-
-
                /* googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
                     @Override
                     public void onMapClick(LatLng latLng) {
@@ -94,10 +114,84 @@ public class MapFragment extends Fragment {
                         googleMap.addMarker(markerOptions);
                     }
                 });*/
+
             }
         });
 
-
+        getCurrentLocationAndDistance();
         return binding.getRoot();
     }
+
+    private void check_If_LocationTurnedOn() {
+
+
+        Toast.makeText(getContext(),"Location Access Neeed",Toast.LENGTH_LONG).show();
+        LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        if (!locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+
+
+            if (ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // Request the location permission if it is not granted
+                ActivityCompat.requestPermissions(getActivity(), new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+            }
+
+            // Location services are disabled, prompt the user to turn them on
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
+        }
+
+    }
+
+
+    private void getCurrentLocationAndDistance() {
+
+
+        if (ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // Request the location permission if it is not granted
+            ActivityCompat.requestPermissions(getActivity(), new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        } else {
+
+            if (locationManager == null) {
+                locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+            }
+
+            // Get the device location
+            locationManager.requestSingleUpdate(LocationManager.NETWORK_PROVIDER, new LocationListener() {
+                @Override
+                public void onLocationChanged(@NonNull Location location) {
+                    // Do something with the location
+                    double latitude = location.getLatitude();
+                    double longitude = location.getLongitude();
+
+                    userLatitude = latitude;
+                    userLongitude = longitude;
+
+
+                    // Create a LatLng object for the user's location
+                    LatLng userLatLng = new LatLng(userLatitude, userLongitude);
+
+                    // Calculate the distance between the user's location and the marker using Location.distanceBetween()
+                    float[] distance = new float[1];
+                    Location.distanceBetween(userLatLng.latitude, userLatLng.longitude, lodhaMallLatLng.latitude, lodhaMallLatLng.longitude, distance);
+
+                    // Convert the distance to kilometers
+                    float distanceInKm = distance[0] / 1000;
+
+                    System.out.println("Distance:"+ distanceInKm);
+                    // Log the distance
+                    Log.d("MapsActivity", "Distance from user to Lodha Xperia Mall: " + distanceInKm + " km");
+
+
+
+                }
+
+            }, null);
+        }
+
+    }
+
 }
